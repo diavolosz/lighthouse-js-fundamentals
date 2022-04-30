391e796b81a55029f9b9a8af89cb7e0e71d8196b
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-04-30 22:24:30 +0000

    added while loop in packing list excercise

--- packing-list.js
+++ packing-list.js
@@ -3,10 +3,16 @@
 
 
 
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils", "tissue papers"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
